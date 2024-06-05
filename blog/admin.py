4c3ad8453c50d627cd3b7cdf3916c18b8c671eeb
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-05 13:00:51 +0330

    created post model and add to admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'date_modified',)
+    search_fields = ('title', 'status', 'date_modified',)
+    ordering = ('status',)
+
+
+admin.site.register(Post, PostAdmin)
