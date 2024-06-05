846ee9c603945314083b0a741c6b02d780894f53
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-05 14:32:58 +0330

    add detail_post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.posts_list, name='posts_list'),
+    path('<int:pk>/', views.post_detail, name='post_detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
 
@@ -11,3 +13,11 @@ def posts_list(request):
         'posts_list': posts,
     }
     return render(request, 'blog/posts_list.html', context)
+
+
+def post_detail(request, pk):
+    post_details = get_object_or_404(Post, pk=pk)
+    context = {
+        'post_details': post_details,
+    }
+    return render(request, 'blog/post_detail.html', context)
