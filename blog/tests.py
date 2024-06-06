afb9e5a9c04c7ef76d8cca62bee195a3978cea59
amirhossein dezhamkhah <dezhamkhah@gmail.com>
2024-06-06 13:59:39 +0330

    add test

--- blog/tests.py
+++ blog/tests.py
@@ -67,7 +67,70 @@ class BlogPostTest(TestCase):
 
     # 8
     # تست اینکخ پست های پیش نویس در صفحه نباشد
-    def test_draf_post_not_show_on_posts_list(self):
+    def test_draft_post_not_show_on_posts_list(self):
         response = self.client.get(reverse('posts_list'))
         self.assertContains(response, self.post1.title)
         self.assertNotContains(response, self.post2.title)
+
+    # 9
+    # تست اینکه در مدل پست ها بر اساس عنوان نمایش داده شود --str--
+    def test_post_model_str(self):
+        post = self.post1
+        self.assertEqual(str(post), post.title)
+
+    # 10
+    # تست اینکه همون چیزی که ذخیره میشه
+    def test_post_detail(self):
+        self.assertEqual(self.post1.title, 'title1')
+        self.assertEqual(self.post1.text, 'this is text1')
+
+    # 11
+    # بررسی کنیم که ایا پست ساخته میشه یا نه
+    def test_post_create_view(self):
+        response = self.client.post(reverse('add_post'), {
+            'title': 'some title',
+            'text': 'this is some text',
+            'status': 'pub',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'some title')
+        self.assertEqual(Post.objects.last().text, 'this is some text')
+
+
+    # 12
+    # بررسی اینکه ایا پست ویرایش میشود
+    def test_post_update_view(self):
+        response = self.client.post(reverse('edit_post', args=[self.post2.id]), {
+            'title': "post 2 updated",
+            'text': "text update",
+            'status': 'pub',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, "post 2 updated")
+        self.assertEqual(Post.objects.last().text, "text update")
+
+    # 13
+    # بررسی اینکه پست حذف میشود یا نه
+    def test_post_delete_view(self):
+        response = self.client.post(reverse('delete_post', args=[self.post1.id]))
+        self.assertEqual(response.status_code, 302)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
